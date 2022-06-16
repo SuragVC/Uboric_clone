8adb6391f23d88ecfcb55d105d0748f57d90aebc
RedStar2255 <Suragvc2000@gmail.com>
2022-06-16 21:37:03 +0530

    cart data appended but not completely functional

--- scripts/cart.js
+++ scripts/cart.js
@@ -8,13 +8,15 @@ lower_div.innerHTML = product_nav()
 
 
 let data = JSON.parse(localStorage.getItem("cart_data"))
-console.log(data)
+let total_price = document.querySelector("#price_div")
 
 let data_appender = document.getElementById("data_appender")
-let counter = 0;
+let counter = 1;
+DisplayCart()
+totalShower()
 
 function DisplayCart() {
-    data.forEach(function(elem) {
+    data.forEach(function(elem, index) {
         let main_div = document.createElement("div")
 
         let div1 = document.createElement("div")
@@ -23,55 +25,82 @@ function DisplayCart() {
         div1.append(img)
 
         let div2 = document.createElement("div")
-        let name = document.createElement("p")
-        name.innerText = data.name
+        let name = document.createElement("h4")
+        name.innerText = elem.name
         div2.append(name)
 
         let div3 = document.createElement("div")
         let price = document.createElement("h3")
-        price.innerText = data.price
+        price.innerText = elem.price
         div3.append(price)
 
         let div4 = document.createElement("div")
+        div4.setAttribute("class", "div4_cart")
+
+
+
+        let div4_inside2 = document.createElement("div")
+        let count_of_Qty = document.createElement("h4")
+        count_of_Qty.innerText = counter
+
+        div4_inside2.append(count_of_Qty)
 
         let div4_inside1 = document.createElement("div")
-
-        let add_button = document.createElement("p")
+        let add_button = document.createElement("h4")
         add_button.innerText = "+"
         div4_inside1.append(add_button)
         div4_inside1.addEventListener("click", function() {
-            addQty(counter)
+            counter++
+            addQty(counter, elem, count_of_Qty)
         })
 
-        let div4_inside2 = document.createElement("div")
-        let count_of_Qty = document.createElement("p")
-        if (counter == 0) {
-            count_of_Qty.innerText = "1"
-        } else {
-            count_of_Qty.innerText = counter
-        }
-        div4_inside2.append(count_of_Qty)
 
         let div4_inside3 = document.createElement("div")
 
-        let remove_button = document.createElement("p")
+        let remove_button = document.createElement("h4")
         remove_button.innerText = "-"
         div4_inside3.append(remove_button)
         div4_inside3.addEventListener("click", function() {
+
             removeQty(counter)
         })
 
         div4.append(div4_inside1, div4_inside2, div4_inside3)
 
 
+        let div5 = document.createElement("div")
+        div5.setAttribute("class", "remove_btn_div")
+        let remove_product = document.createElement("input")
+        remove_product.type = "submit"
+        remove_product.value = "REMOVE"
+
+        remove_product.setAttribute("class", "remove_product")
+        remove_product.addEventListener("click", function() {
+            removeProduct(elem)
+        })
+        div5.append(remove_product)
+
+        main_div.append(div1, div2, div3, div4, div5)
+        data_appender.append(main_div)
     })
 }
 
 function addQty() {
 
+}
+
+
+
+function removeQty(counter) {
 
 }
 
-function removeQty(counter) {
+function removeProduct() {
+
+}
+
+function totalShower() {
+    total_price.innerHTML = null
+    let total_amt = document.createElement("h3")
 
 }
