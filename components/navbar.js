f8911bbb3ef79c96c34986a16a13001c34807078
Rohit Singh <kailash.jc.rs@gmail.com>
2022-06-17 18:22:23 +0530

    navbar menu updated

--- components/navbar.js
+++ components/navbar.js
@@ -24,11 +24,11 @@ function navbar() {
 
 function product_nav() {
    return `    <div id="categories_div">
-    <a  id="product_link" href="index.html"><h3>HOME</h3></a>
+    <a  id="product_link" href="index.html"><h3>Home</h3></a>
     <a id="product_link" href="kitchentools.html"><h3>Products</h3></a>
     <a id="product_link" href="user.html"><h3>Customer/Guest login</h3></a>
-    <h3>Merchant/Bulk Order</h3>
-    <h3>Become A Seller</h3>
+    <a id="product_link" href="#"><h3>Merchant/Bulk Order</h3></a>
+    <a id="product_link" href="#"><h3>Become A Seller</h3></a>
  
  </div>`
 }
